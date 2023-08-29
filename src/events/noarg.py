ec4f60cf4be40da0797972210a882f6f388d26d3
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-29 13:45:40 +1000

    removing error parsing from noarg class

--- src/events/noarg.py
+++ src/events/noarg.py
@@ -6,10 +6,3 @@ class NoArg(Event):
     @abstractmethod
     def __init__(self, name):
         super().__init__(name, 0)
-    
-    @abstractmethod
-    def tokenise(self, data : str) -> list[str]:
-        tokens : list[str] = super().tokenise(data)
-        if tokens:
-            raise ValueError(f"{self.name} expects no arguments")
-        return tokens
