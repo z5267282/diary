aa1824dd25bb7d2b55b2aa445989c70f54adc52b
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-29 14:11:17 +1000

    adding append support for no arg events

--- src/events/abstract/noarg.py
+++ src/events/abstract/noarg.py
@@ -7,3 +7,7 @@ class NoArg(Event):
     def __init__(self, name):
         """No arg events are to be run without arguments"""
         super().__init__(name, 0)
+    
+    def append(self, old: list, entry: dict) -> None:
+        """No arg events should only store their date"""
+        old.append(entry["date"])
