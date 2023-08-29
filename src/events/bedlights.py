63f6716dc16eb73bd183a6179933de17434c90db
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-29 13:52:15 +1000

    adding bedlights command

---
+++ src/events/bedlights.py
@@ -0,0 +1,5 @@
+from noarg import NoArg
+
+class BedLights(NoArg):
+    def __init__(self):
+        super().__init__("bed lights")
--- src/events/noarg.py
+++ src/events/noarg.py
@@ -5,4 +5,5 @@ from event import Event
 class NoArg(Event):
     @abstractmethod
     def __init__(self, name):
+        """No arg events are to be run without arguments"""
         super().__init__(name, 0)
