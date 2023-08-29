5c23ff7b936815ba917db1e00f310c5e8150cbd6
Sunny Chen <z5267282@student.unsw.edu.au>
2023-08-30 00:35:44 +1000

    adding other commands to main

--- src/main.py
+++ src/main.py
@@ -4,13 +4,16 @@ from error import NO_INPUT, NO_COMMAND
 
 from events.abstract.event import Event
 
+# no arg
 from events.concrete.bedlights import BedLights
 from events.concrete.shave import Shave
+# one arg
+from events.concrete.haircut import HairCut
+from events.concrete.note import Note
+from events.concrete.ping import Ping
 
 def main():
-    events : list[Event] = [
-        BedLights(), Shave()
-    ]
+    events : list[Event] = create_events()
     mapping : dict[str, Event] = {
         event.get_shorthand() : event for event in events
     }
@@ -34,6 +37,11 @@ def main():
         sys.exit(NO_COMMAND)
     
     event.dump(args)
+
+def create_events():
+    no_arg : list[Event] = [ BedLights(), Shave() ]
+    one_arg : list[Event] = [ HairCut(), Note(), Ping() ]
+    return no_arg + one_arg
 
 if __name__ == "__main__":
     main()
